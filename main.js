6395696e8edf376c282a56409d0eebd6781c13b5
Vanilton-Jr <remove>
2022-10-26 17:33:36 -0300

    ok

--- main.js
+++ main.js
@@ -8,46 +8,24 @@ function cores() {
     var test = document.getElementById("test_txt");
     var color = document.getElementById("color_pick");
     var msg = document.getElementsByClassName("cliente");
-    test.innerText = color.value;
-    color.value
 
     for (var i = 0; i < msg.length; i++) {
-        msg[i].style.backgroundColor = hexToRgbA(color.value);
+        msg[i].style.backgroundColor = color.value;
     }
-}
-
-function hexToRgbA(hex) {
-    var c;
-    if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
-        c = hex.substring(1).split('');
-        if (c.length == 3) {
-            c = [c[0], c[0], c[1], c[1], c[2], c[2]];
-        }
-        c = '0x' + c.join('');
-        return 'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ',0.9)';
-    }
-    throw new Error('Bad Hex');
 }
 
 function renderMenssagemUsuario() {
-    if (chave == 1) {
-        var msg = document.getElementById('barra_menssagem'); //le oque está escrito na barra
-        if (msg.value.length >= 1 && msg.value.length <= 199) {
-            const msg_usuario = msg.value;
-            adMens('cliente', `${msg_usuario}`);
-            renderMenssagemEmpresa(msg_usuario);
-        }
-
-        if (msg.value.length >= 199) {
-            alert('Execedeu o limite de Characteres')
-        }
-
-        chave = 0;
+    var msg = document.getElementById('barra_menssagem'); //le oque está escrito na barra
+    if (msg.value.length >= 1 && msg.value.length <= 199) {
+        const msg_usuario = msg.value;
+        adMens('cliente', `${msg_usuario}`);
+        renderMenssagemEmpresa(msg_usuario);
     }
-    while (chave == 0) {
-        setTimeout(() => { chave = 1 }, 2000);
+
+    if (msg.value.length >= 199) {
+        alert('Execedeu o limite de Characteres')
     }
-    console.log(chave)
+
 }
 
 function renderMenssagemEmpresa(msg_usuario) {
@@ -99,16 +77,25 @@ document.onkeydown = function (e) {// permite acionar a função através da tec
     }
 };
 
-//  ---------------------------------------RESPONDER-----------------------------------------------------4
+//  ---------------------------------------RESPONDER-----------------------------------------------------
 
-const respostaObj = {
-    olá: "Olá tudo bem?",
-    hoje: new Date().toDateString(),
-    "tud bien": "não marcelo eu não estou"
-};
+function criarPergunta(i){
+   let fnew = document.createElement('var');
+   console.log
 
-const pegarResposta = (msg) => {
-    return respostaObj[msg] == undefined
-        ? "Tente outra pergunta"
-        : respostaObj[msg]
+}
+
+let hoje = "hoje tem"
+function pegarResposta(msg) {
+    var Resposta;
+    switch(msg) {
+        case hoje:
+             Resposta = new Date().toDateString();
+             break;
+        case ontem:
+            Resposta = new Date().toDateString();
+            break;
+            default: Resposta = "Tente outra pergunta";
+    }
+    return Resposta;
 }
